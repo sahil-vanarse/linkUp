580c5684f16b6dd6bb4b4429eab429cf2ff4a835
sahil-vanarse <sahilvanarse13@gmail.com>
2024-09-18 00:06:39 +0530

    Registration Authentication

--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # rooms = [
 #     {'id' : 1, 'name' : 'Lets learn python'},
@@ -14,11 +15,12 @@ from django.contrib import messages
 #     {'id' : 3, 'name' : 'frontend developer'}
     
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -34,12 +36,27 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or Password does not exist')
 
-    context = {}
+    context = {'page' : page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    # page = 'Register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+    return render(request, 'base/login_register.html', {'form' : form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -56,8 +73,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    
-    context = {'room' : room}
+    messages = room.message_set.all()
+    context = {'room' : room, 'messages' : messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url="login")
