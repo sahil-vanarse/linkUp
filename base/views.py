d69d057a5b19c2b7e88eb6cf4c6a7ac9a3347497
sahil-vanarse <sahilvanarse13@gmail.com>
2024-10-29 01:50:03 +0530

    Messages

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -73,8 +73,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    messages = room.message_set.all()
-    context = {'room' : room, 'messages' : messages}
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room' : room, 'room_messages' : room_messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url="login")
