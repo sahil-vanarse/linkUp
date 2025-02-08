d0b00d87e1d69d8f093741eb9cb4be9d85dfc0dc
sahil-vanarse <sahilvanarse13@gmail.com>
2025-02-08 20:02:57 +0530

    Made the changes

--- base/models.py
+++ base/models.py
@@ -81,7 +81,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True, db_index=True)
     username = models.CharField(max_length=200, null=True, blank=True, unique=True)  # Make username optional and unique
     bio = models.TextField(null=True, default="Hey there! I'm using this app.")
-    avatar = models.ImageField(null=True, default="avatar.svg", storage=RawMediaCloudinaryStorage(), upload_to='avatars/')  # here changed
+    avatar = models.ImageField(null=True, blank=True, default="avatar.svg", storage=RawMediaCloudinaryStorage(), upload_to='avatars/')  # here changed
     search_vector = SearchVectorField(null=True)
 
     USERNAME_FIELD = 'email'
@@ -97,6 +97,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+    def get_avatar_url(self):
+        if self.avatar and hasattr(self.avatar, 'url'):
+            return self.avatar.url
+        return 'path/to/default/avatar.svg'  # Replace with your default avatar path
 
 
 
